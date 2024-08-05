8c6fd261d165f01d00ab757372cbb56b81150e7f
rihamkatout <rihamkatm@gmail.com>
2024-08-05 11:31:46 +0300

    connect to DB

--- section3/src/main/java/com/exalt/section3/Section3Application.java
+++ section3/src/main/java/com/exalt/section3/Section3Application.java
@@ -1,7 +1,9 @@
 package com.exalt.section3;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Section3Application {
@@ -10,4 +12,10 @@ public class Section3Application {
 		SpringApplication.run(Section3Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String []args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }

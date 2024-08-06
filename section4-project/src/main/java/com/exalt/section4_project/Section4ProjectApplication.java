9fb6fdab17958187a176a4af29471bc556af7a8b
Riham Katout <rihamkatm@gmail.com>
2024-08-06 15:14:53 +0300

    init section4-project

---
+++ section4-project/src/main/java/com/exalt/section4_project/Section4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exalt.section4_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4ProjectApplication.class, args);
+	}
+
+}

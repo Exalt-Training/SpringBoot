253b8f2fa71bebe6d00d634f740fcee2132dc191
Riham Katout <rihamkatm@gmail.com>
2024-08-07 13:07:55 +0300

    restrict according to role

--- section4-project/src/main/java/com/exalt/section4_project/security/DemoSecurityConfig.java
+++ section4-project/src/main/java/com/exalt/section4_project/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.exalt.section4_project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +35,26 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(riham, siwar, ahmad);
         //Since we defined our users here, spring will not use users in properties file
+    }
+
+    // For restricting access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                    .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }

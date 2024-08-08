70b8051124603d4274d2d5c143d696f0cfd522f4
Riham Katout <rihamkatm@gmail.com>
2024-08-08 14:55:11 +0300

    adding controller

---
+++ section6/src/main/java/com/exalt/section6/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.exalt.section6.controller;
+
+import org.springframework.stereotype.Controller;
+// be sure to use this import
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+        model.addAttribute("theDate", new java.util.Date());
+        return "helloword"; // --> src/main/resources/templates/helloworld.html
+    }
+}

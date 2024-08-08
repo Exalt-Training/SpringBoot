4da3644113c7ff196451642a22b4c9aa0341b222
Riham Katout <rihamkatm@gmail.com>
2024-08-08 14:15:08 +0300

    init the project

---
+++ section6/src/test/java/com/exalt/section6/Section6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalt.section6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

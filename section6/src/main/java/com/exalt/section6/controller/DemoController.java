bb640c26bb0d1db0e8c66383db5edbf6bfa47243
Riham Katout <rihamkatm@gmail.com>
2024-08-08 15:09:49 +0300

    adding thymeleaf template

--- section6/src/main/java/com/exalt/section6/controller/DemoController.java
+++ section6/src/main/java/com/exalt/section6/controller/DemoController.java
@@ -11,6 +11,6 @@ public class DemoController {
     @GetMapping("/hello")
     public String sayHello(Model model){
         model.addAttribute("theDate", new java.util.Date());
-        return "helloword"; // --> src/main/resources/templates/helloworld.html
+        return "helloworld"; // --> src/main/resources/templates/helloworld.html
     }
 }

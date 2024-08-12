7939fab74f8da93f124b19601b4165b961f7dd8a
rihamkatout <rihamkatm@gmail.com>
2024-08-12 13:25:22 +0300

    init section8 project

---
+++ section8/src/main/java/com/exalt/section8/Section8Application.java
@@ -0,0 +1,13 @@
+package com.exalt.section8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section8Application.class, args);
+	}
+
+}
---
+++ section8/src/test/java/com/exalt/section8/Section8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalt.section8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

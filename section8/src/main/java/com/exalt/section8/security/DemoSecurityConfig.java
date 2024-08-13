0174c750864246a71b9a0fb7f17d4e7d98db01e4
rihamkatout <rihamkatm@gmail.com>
2024-08-13 11:21:29 +0300

    display username and roles

--- section8/src/main/java/com/exalt/section8/security/DemoSecurityConfig.java
+++ section8/src/main/java/com/exalt/section8/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.exalt.section8.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -43,7 +44,7 @@ public class DemoSecurityConfig {
                         form.loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
-                );
+                ).logout(LogoutConfigurer::permitAll);
         return httpSecurity.build();
     }
 }
